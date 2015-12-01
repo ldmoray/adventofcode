b66b935c8354342aa3bcd92ff4fd633946be5c7c
Lenny Morayniss <ldmoray@gmail.com>
2015-12-01 18:16:55 -0500

    Why were these tabs?

--- day1_1.py
+++ day1_1.py
@@ -2,37 +2,37 @@ import argparse
 
 
 def count_parens(string):
-	count = 0
-	for char in string:
-		if char == '(':
-			count += 1
-		elif char == ')':
-			count -= 1
-		elif char != '\n':
-			raise ValueError('Invalid Character in Pattern: %s' % char)
-	return count
+    count = 0
+    for char in string:
+        if char == '(':
+            count += 1
+        elif char == ')':
+            count -= 1
+        elif char != '\n':
+            raise ValueError('Invalid Character in Pattern: %s' % char)
+    return count
 
 
 def main():
-	parser = argparse.ArgumentParser(description='Solve the day 1 challenges for Advent of Code')
-	parser.add_argument('pattern', help='The pattern of parentheses to solve', default='', nargs='?')
-	parser.add_argument('-f', '--file', help='Read the pattern from a file instead of from the command line')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Solve the day 1 challenges for Advent of Code')
+    parser.add_argument('pattern', help='The pattern of parentheses to solve', default='', nargs='?')
+    parser.add_argument('-f', '--file', help='Read the pattern from a file instead of from the command line')
+    args = parser.parse_args()
 
-	pattern = args.pattern
-	if args.file:
-		try:
-			with open(args.file, 'r') as f:
-				pattern = f.next()
-		except IOError as err:
-			print 'There was an error reading from disk: %s' % err.strerror
-			exit()
-	try:
-		floor_count = count_parens(pattern)
-		print 'Floor: %d' % floor_count
-	except ValueError as err:
-		print str(err)
+    pattern = args.pattern
+    if args.file:
+        try:
+            with open(args.file, 'r') as f:
+                pattern = f.next()
+        except IOError as err:
+            print 'There was an error reading from disk: %s' % err.strerror
+            exit()
+    try:
+        floor_count = count_parens(pattern)
+        print 'Floor: %d' % floor_count
+    except ValueError as err:
+        print str(err)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- day1_2.py
+++ day1_2.py
@@ -1,18 +1,18 @@
 def main():
-	# load input from file
-	floor = 0
-	count = 0
-	with open('day1_1.txt', 'r') as f:
-		line = f.next()
-		for char in line:
-			if char == '(':
-				floor += 1
-			elif char == ')':
-				floor -= 1
-			count += 1
-			if floor < 0:
-				print "Count %d" % count
-				break
+    # load input from file
+    floor = 0
+    count = 0
+    with open('day1_1.txt', 'r') as f:
+        line = f.next()
+        for char in line:
+            if char == '(':
+                floor += 1
+            elif char == ')':
+                floor -= 1
+            count += 1
+            if floor < 0:
+                print "Count %d" % count
+                break
 
 if __name__ == '__main__':
-	main()
+    main()
