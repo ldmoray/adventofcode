591f559c5ec677212952c29acc41a1bb5ca64271
Lenny Morayniss <ldmoray@gmail.com>
2015-12-02 13:04:24 -0500

    Part 2 Done

--- day2/day2.py
+++ day2/day2.py
@@ -6,42 +6,69 @@ def split_pattern(pattern):
     return [int(x) for x in pattern]
 
 
+def ribbon_length(length, width, height):
+    front = 2 * (length + width)
+    side = 2 * (width + height)
+    top = 2 * (length + height)
+    return min(front, side, top) + length * width * height
+
+
 def slack_surface_area(length, width, height):
-    front = length * width
-    side = width * height
-    top = length * height
-    slack = min(front, side, top)
-    area = 2 * front + 2 * side + 2 * top
-    total = area + slack
-    return total
+    front_a = length * width
+    side_a = width * height
+    top_a = length * height
+    return 2 * front_a + 2 * side_a + 2 * top_a + min(front_a, side_a, top_a)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Solve the day 2 challenges for Advent of Code')
     parser.add_argument('pattern', help='The pattern that represents the shape of a box to solve', default='', nargs='?')
     parser.add_argument('-f', '--file', help='Read patterns from a file instead of from the command line')
+    parser.add_argument('-r', '--ribbon', help='Find the amount of ribbon to order.', action='store_true')
     args = parser.parse_args()
 
-    area = 0
-    if args.file:
-        try:
-            with open(args.file, 'r') as f:
-                for pattern in f:
-                    dimensions = split_pattern(pattern)
-                    area += slack_surface_area(*dimensions)
-            print 'Total Area with slack is %d' % area
-        except IOError as err:
-            print 'There was an error reading from disk: %s' % err.strerror
-            exit()
-    elif args.pattern:
-        try:
-            dimensions = split_pattern(args.pattern)
-            area = slack_surface_area(*dimensions)
-            print 'Total Area with slack is %d' % area
-        except ValueError as err:
-            print str(err)
+    if args.ribbon:
+        length = 0
+        if args.file:
+            try:
+                with open(args.file, 'r') as f:
+                    for pattern in f:
+                        dimensions = split_pattern(pattern)
+                        length += ribbon_length(*dimensions)
+                print 'Total Length of Ribbon is %d' % length
+            except IOError as err:
+                print 'There was an error reading from disk: %s' % err.strerror
+                exit()
+        elif args.pattern:
+            try:
+                dimensions = split_pattern(args.pattern)
+                length = ribbon_length(*dimensions)
+                print 'Total Length of Ribbon is %d' % length
+            except ValueError as err:
+                print str(err)
+        else:
+            print 'Please enter a pattern'
     else:
-        print 'Please enter a pattern'
+        area = 0
+        if args.file:
+            try:
+                with open(args.file, 'r') as f:
+                    for pattern in f:
+                        dimensions = split_pattern(pattern)
+                        area += slack_surface_area(*dimensions)
+                print 'Total Area with slack is %d' % area
+            except IOError as err:
+                print 'There was an error reading from disk: %s' % err.strerror
+                exit()
+        elif args.pattern:
+            try:
+                dimensions = split_pattern(args.pattern)
+                area = slack_surface_area(*dimensions)
+                print 'Total Area with slack is %d' % area
+            except ValueError as err:
+                print str(err)
+        else:
+            print 'Please enter a pattern'
 
 
 if __name__ == '__main__':
